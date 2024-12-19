7c5b84abf10e7615cb4b14c25e1c7bece82c3660
Mgxxxx <aminebengelloun@gmail.com>
2024-12-19 02:09:05 +0100

    80 epochs

--- src/config.py
+++ src/config.py
@@ -7,6 +7,6 @@ TEST_IMAGES_PATH = DATA_PATH + "test_set_images/"
 # Hyperparameters
 BATCH_SIZE = 16
 LEARNING_RATE = 1e-4
-EPOCHS = 50
+EPOCHS = 80
 DEVICE = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using device: {DEVICE}")
